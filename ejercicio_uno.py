144d4817a667dbee75d817ff578a8dcb9f744d83
geanpiere27_github <geanpiereh2005@gmail.com_github>
2024-07-17 09:49:58 -0500

    solucion:primer ejercicio

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,11 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+list_numeros = [5,8,9,6,2,10]
+def lista(numeros):
+    menor = min(numeros)
+    mayor = max(numeros)
+
+    return {f"numero menor": menor, "numero mayor": mayor}
+salida = lista(list_numeros)
+print(salida)  
