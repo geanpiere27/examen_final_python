4674f7bb13abddac8ad4e5a9a14769951e0b3284
geanpiere27_github <geanpiereh2005@gmail.com_github>
2024-07-17 10:31:32 -0500

    solucion:ejercicio tres

--- ejercicio_tres.py
+++ ejercicio_tres.py
@@ -4,3 +4,9 @@ ejm:
 entrada: eucalipto
 salida: {e:1,u:1,a:1,i:1,o:1}
 """
+def contar_vocales(texto):
+    vocales = "aeiou"
+    return {vocal: texto.count(vocal) for vocal in vocales if vocal in texto}
+texto = "arbol"
+resultado = contar_vocales(texto)
+print(resultado) 
